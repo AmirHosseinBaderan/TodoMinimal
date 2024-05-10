d3009c8b207ab7c934208f0278a40ad10bc052ab
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-11 01:14:17 +0330

    create : get todos query handler

---
+++ TodoMinimal/Src/Application/Todo.Application/Todo/GetTodos/GetTodoQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+using MediatR;
+
+namespace Application.Todo.GetTodos;
+
+public class GetTodoQueryHandler(IBaseQuery<Domain.Aggregates.Todo> query) : IRequestHandler<GetTodosQuery, IEnumerable<TodoDto>>
+{
+    public async Task<IEnumerable<TodoDto>> Handle(GetTodosQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<Domain.Aggregates.Todo> todos = await query.GetAllAsync();
+        return todos.Select(t => (TodoDto)t);
+    }
+}
