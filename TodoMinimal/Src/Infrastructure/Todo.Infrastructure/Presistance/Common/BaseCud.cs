3873a044857e7bfbda513e3daf4f203693b48163
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-10 22:41:07 +0330

    create : base service implementation

---
+++ TodoMinimal/Src/Infrastructure/Todo.Infrastructure/Presistance/Common/BaseCud.cs
@@ -0,0 +1,7 @@
+﻿using Todo.Domain.Common;
+
+namespace Todo.Infrastructure.Presistance;
+
+public class BaseCud<TEntity> : IBaseCud<TEntity> where TEntity : BaseEntity
+{
+}
---
+++ TodoMinimal/Src/Infrastructure/Todo.Infrastructure/Presistance/Common/BaseQuery.cs
@@ -0,0 +1,7 @@
+﻿using Todo.Domain.Common;
+
+namespace Todo.Infrastructure.Presistance;
+
+public class BaseQuery<TEntity> : IBaseQuery<TEntity> where TEntity : BaseEntity
+{
+}
