3348d9aba0630b96f4f6ec6c12b42124eb4b4f4c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-11 00:26:27 +0330

    create : dependency injection and mapper

---
+++ TodoMinimal/Src/Server/Todo.Server.Api/DependencyInjections.cs
@@ -0,0 +1,24 @@
+﻿using Mapster;
+using MapsterMapper;
+using System.Reflection;
+
+namespace Todo.Server.Api;
+
+public static class DependencyInjections
+{
+    private readonly static Assembly[] Assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+    public static IServiceCollection ConfigureMapster(this IServiceCollection services)
+    {
+        var typeAdaperConfig = TypeAdapterConfig.GlobalSettings;
+        typeAdaperConfig.Scan(Assemblies);
+
+        services.AddSingleton(typeAdaperConfig);
+        services.AddScoped<IMapper, ServiceMapper>();
+
+        return services;
+    }
+
+
+
+}
