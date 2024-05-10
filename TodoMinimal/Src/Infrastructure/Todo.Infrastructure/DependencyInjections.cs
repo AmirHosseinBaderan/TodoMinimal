59ed6b6fedde6a2a6c99ef0c34e9f59247e2f931
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-10 22:40:55 +0330

    create : dependency injection layer

---
+++ TodoMinimal/Src/Infrastructure/Todo.Infrastructure/DependencyInjections.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.Domain.Common;
+using Todo.Infrastructure.Presistance;
+
+namespace Todo.Infrastructure;
+
+public static class DependencyInjections
+{
+    public static IServiceCollection ConfigureInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddDbContext<TodoDbContext>(options => options
+                                                   .UseSqlServer(configuration
+                                                   .GetConnectionString(TodoDbContextSchema
+                                                                        .DefaultConnectionStringName)));
+
+        services.AddScoped(typeof(IBaseCud<>), typeof(BaseCud<>));
+        services.AddScoped(typeof(IBaseQuery<>), typeof(BaseQuery<>));
+
+        return services;
+    }
+}
