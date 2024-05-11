1b937b3c034f1388be0abc83eab547811b11f4b5
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-11 12:01:02 +0330

    create : action status handler for todo

---
+++ TodoMinimal/Src/Server/Todo.Server.Api/Endpoints/Todo/ActionStatusHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Todo;
+
+namespace Server.Api.Endpoints.Todo;
+
+public static class TodoActionStatusHandler
+{
+    public static IResponse Handler(this TodoActionStatus status, object? result)
+    {
+        Response response = new();
+        return status switch
+        {
+            TodoActionStatus.Success => response.Success(result ?? new()),
+            TodoActionStatus.Faild => response.Faild(500, "Exception in action"),
+            TodoActionStatus.NotFound => response.Faild(404, "Todo not found"),
+            _ => response.Exception(),
+        };
+    }
+}
