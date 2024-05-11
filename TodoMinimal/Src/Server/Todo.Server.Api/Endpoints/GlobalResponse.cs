80292e65733d3134261c38b390cdcfae62b5da1a
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-11 12:00:54 +0330

    Create GlobalResponse.cs

---
+++ TodoMinimal/Src/Server/Todo.Server.Api/Endpoints/GlobalResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Server.Api.Endpoints;
+
+public static class GlobalResponse
+{
+    public static IResponse Success(this object result)
+    {
+        Response response = new();
+        return response.Success(result);
+    }
+}
