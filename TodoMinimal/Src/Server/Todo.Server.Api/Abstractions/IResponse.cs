7124b0e6512c907258ff9ce8ae7f8c43d1bda8a3
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-11 12:00:49 +0330

    create : response for endpoint

---
+++ TodoMinimal/Src/Server/Todo.Server.Api/Abstractions/IResponse.cs
@@ -0,0 +1,53 @@
+﻿namespace Server.Api.Abstractions;
+
+public interface IResponse
+{
+    public bool Status { get; set; }
+
+    public int Code { get; set; }
+
+    public string Message { get; set; }
+
+    public object Result { get; set; }
+
+    public IResponse Success(object result, string message = "");
+
+    public IResponse Faild(int code, string message);
+
+    public IResponse Exception();
+}
+
+public class Response : IResponse
+{
+    public bool Status { get; set; }
+    public int Code { get; set; }
+    public string Message { get; set; } = null!;
+    public object Result { get; set; } = null!;
+
+    public IResponse Exception()
+        => new Response
+        {
+            Code = 500,
+            Result = new(),
+            Status = false,
+            Message = "Internal Server Error",
+        };
+
+    public IResponse Faild(int code, string message)
+        => new Response
+        {
+            Code = code,
+            Result = new(),
+            Status = false,
+            Message = message,
+        };
+
+    public IResponse Success(object result, string message = "")
+        => new Response
+        {
+            Code = 200,
+            Message = message,
+            Result = result,
+            Status = true,
+        };
+}
