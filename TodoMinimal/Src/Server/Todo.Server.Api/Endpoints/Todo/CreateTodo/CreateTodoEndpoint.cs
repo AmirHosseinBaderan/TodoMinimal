d711f6eca00da8b9153fc4a0b6311a012688e58e
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-11 12:01:14 +0330

    update : use new way for response

--- TodoMinimal/Src/Server/Todo.Server.Api/Endpoints/Todo/CreateTodo/CreateTodoEndpoint.cs
+++ TodoMinimal/Src/Server/Todo.Server.Api/Endpoints/Todo/CreateTodo/CreateTodoEndpoint.cs
@@ -4,18 +4,16 @@ using Todo.Server.Api.Endpoints;
 
 namespace Server.Api.Endpoints.Todo.CreateTodo;
 
-public class CreateTodoEndpoint : IEndpoint, IEndpointHandler<CreateTodoRequest, CreateTodoResponse>
+public class CreateTodoEndpoint : IEndpoint, IEndpointHandler<CreateTodoRequest, IResponse>
 {
-    public async Task<CreateTodoResponse> HandlerAsync(CreateTodoRequest request, IMapper mapper, IMediator mediator)
+    public async Task<IResponse> HandlerAsync(CreateTodoRequest request, IMapper mapper, IMediator mediator)
     {
         CreateTodoCommand commnad = mapper.Map<CreateTodoCommand>(request);
         var result = await mediator.Send(commnad);
 
         return result
-                .Match(Left: (status) =>
-                      new CreateTodoResponse(false, null),
-                      Right: (todo) =>
-                      new CreateTodoResponse(true, todo));
+                .Match(Left: (status) => status.Handler(null),
+                      Right: (todo) => todo.Success());
     }
 
     public void MapEndpoint(IEndpointRouteBuilder app)
--- TodoMinimal/Src/Server/Todo.Server.Api/Endpoints/Todo/UpdateTodo/UpdateTodoEndpoint.cs
+++ TodoMinimal/Src/Server/Todo.Server.Api/Endpoints/Todo/UpdateTodo/UpdateTodoEndpoint.cs
@@ -1,16 +1,20 @@
 ﻿
 
+using Application.Todo.UpdateTodo;
 using Server.Api.Filters;
 using Todo.Server.Api.Endpoints;
 
 namespace Server.Api.Endpoints.Todo.UpdateTodo;
 
-public class UpdateTodoEndpoint : IEndpoint, IEndpointHandler<UpdateTodoRequest, UpdateTodoResponse>
+public class UpdateTodoEndpoint : IEndpoint, IEndpointHandler<UpdateTodoRequest, IResponse>
 {
-    public async Task<UpdateTodoResponse> HandlerAsync(UpdateTodoRequest request, IMapper mapper, IMediator mediator)
+    public async Task<IResponse> HandlerAsync(UpdateTodoRequest request, IMapper mapper, IMediator mediator)
     {
-        var command = await mediator.Send(request);
-        return new(true, null);
+        var command = mapper.Map<UpdateTodoCommand>(request);
+        var result = await mediator.Send(command);
+
+        return result.Match(Left: (status) => status.Handler(null),
+                            Right: (todo) => todo.Success());
     }
 
     public void MapEndpoint(IEndpointRouteBuilder app)
