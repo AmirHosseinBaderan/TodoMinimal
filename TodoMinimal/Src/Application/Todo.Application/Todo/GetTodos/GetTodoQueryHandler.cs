39117b7b832a64ef9aa72d24600bcbf4852e5900
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-05-25 12:08:56 +0330

    Update GetTodoQueryHandler.cs

--- TodoMinimal/Src/Application/Todo.Application/Todo/GetTodos/GetTodoQueryHandler.cs
+++ TodoMinimal/Src/Application/Todo.Application/Todo/GetTodos/GetTodoQueryHandler.cs
@@ -1,13 +1,10 @@
-﻿using Domain.Common;
-using MediatR;
-
-namespace Application.Todo.GetTodos;
+﻿namespace Application.Todo.GetTodos;
 
 public class GetTodoQueryHandler(IBaseQuery<Domain.Aggregates.Todo> query) : IRequestHandler<GetTodosQuery, IEnumerable<TodoDto>>
 {
     public async Task<IEnumerable<TodoDto>> Handle(GetTodosQuery request, CancellationToken cancellationToken)
     {
-        IEnumerable<Domain.Aggregates.Todo> todos = await query.GetAllAsync();
+        IEnumerable<Domain.Aggregates.Todo> todos = await query.GetAllAsync(request.Page, request.Count);
         return todos.Select(t => (TodoDto)t);
     }
 }
